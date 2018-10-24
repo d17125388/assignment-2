0a18673b3658297f644c531933c51cc60dd8bbaa
Ades77 <32359250+Ades77@users.noreply.github.com>
2018-10-24 18:30:35 +0100

    Add files via upload
    python code for cleansing csv file

---
+++ test-scripts/csvcleanse.py
@@ -0,0 +1,16 @@
+import csv
+
+
+try:
+    with open('csv.txt', newline='', encoding="ISO-8859-1") as file1:
+        file2 = open('file2.txt', 'w')
+        reader = csv.reader(file1)
+        writer = csv.writer(file2)
+        for row in reader:
+            print(row[0],',',row[11],file=file2)
+
+    file1.close()
+    file2.close()
+
+except IOError:
+    print('That file does not exist')
