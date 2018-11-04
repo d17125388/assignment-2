0e643fd572cbc25462c18468704249297a8d33e4
d17125388 <43420057+d17125388@users.noreply.github.com>
2018-11-04 12:19:01 +0000

    Update csvcleanse.py

--- test-scripts/csvcleanse.py
+++ test-scripts/csvcleanse.py
@@ -7,7 +7,9 @@ try:
         reader = csv.reader(file1)
         
         for row in reader:
-            print(row[0],',',row[11],file=file2)
+            # print(row[0],',',row[11],file=file2)
+            # Prevents Python from adding unnecessary spaces
+            print(row[0]+','+row[11],file=file2)
 
     file1.close()
     file2.close()
