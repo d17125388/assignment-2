8dd030215d3c228da5a7aae7d6d41aa3f9160173
andyyingDIT <43420072+andyyingDIT@users.noreply.github.com>
2018-11-14 20:33:54 +0000

    Update flaskproofofconcept.py

--- test-scripts/flaskproofofconcept.py
+++ test-scripts/flaskproofofconcept.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 
 @app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.html") # Is the index.html file supposed to sit in the home directory?
 
 @app.route("/home/api")
 def api():
-    mydict = csvdict.csvcleanse.py  # json
+    mydict = csvdict.csvcleanse.py  # We've tried to put the csvcleanse.py file into an api directory
+                                       # which is also inside the home directory, but when running it, it still gives us "URL Not found"
     return make_response(json.dumps(mydict))
 
 if __name__ == '__main__':
